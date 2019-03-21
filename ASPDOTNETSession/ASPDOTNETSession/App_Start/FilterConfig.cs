ea311c44dd147ed30c0f3ad35d2b4c582b46faec
yolanda <ysimpson@spartaglobal.com>
2019-03-21 16:52:21 +0000

    file added

---
+++ ASPDOTNETSession/ASPDOTNETSession/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDOTNETSession
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
